fbb6ee2ac480b3b182f335ae2c16649d852a3469
RAZYANIK <mdfarhanrazy@gmail.com>
2022-12-30 18:29:10 +0600

    2nd commit

--- pages/components/Instagram.jsx
+++ pages/components/Instagram.jsx
@@ -14,14 +14,8 @@ const Instagram = () => {
             <p className='text-2xl font-bold'>Follow us on Instagram</p>
             <p className='pb-4'>@Nature</p>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
-                {/* <PhotoProvider>
-                    <div className="foo">
-                        <PhotoView src={IgImg1}> */}
-                <InstagramImg socialImg={IgImg1} />
-                {/* </PhotoView>
-                    </div>
-                </PhotoProvider> */}
 
+                <InstagramImg socialImg={IgImg1} />
                 <InstagramImg socialImg={IgImg2} />
                 <InstagramImg socialImg={IgImg3} />
                 <InstagramImg socialImg={IgImg4} />
